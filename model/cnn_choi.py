d18dca129b6f1884f505d5a69ab138673c16964f
cheng ying <chengying@tezign.com>
2019-07-03 18:00:05 +0800

    debug

--- model/cnn_choi.py
+++ model/cnn_choi.py
@@ -125,6 +125,83 @@ class CNN_Choi_Slim(Module):
         self.fc = nn.Linear(64, 10)
 
     def forward(self, x):
+        # x = x.permute(0, 2, 1, 3)
+        # x = self.bn(x)
+        # x = x.permute(0, 2, 1, 3)
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.activate1(x)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.activate2(x)
+        x = self.maxpool2(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.activate3(x)
+        x = self.maxpool3(x)
+
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = self.activate4(x)
+        x = self.maxpool4(x)
+
+        x = self.conv5(x)
+        x = self.bn5(x)
+        x = self.activate5(x)
+        x = self.maxpool5(x)
+
+        x = x.view(x.size(0), -1)
+        # x = self.hidden(x)
+        x = self.fc(x)
+        return x
+
+
+class RCNN_Choi(Module):
+    def __init__(self):
+        super(CNN_Choi_Slim, self).__init__()
+
+        # input block
+        self.bn = nn.BatchNorm2d(96)
+
+        # block1
+        self.conv1 = nn.Conv2d(1, 64, 3, padding=(37, 0, 37, 0))
+
+        self.bn1 = nn.BatchNorm2d(64)
+        self.activate1 = nn.ELU()
+        self.maxpool1 = nn.MaxPool2d((2, 4))
+
+        # block2
+        self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.activate2 = nn.ELU()
+        self.maxpool2 = nn.MaxPool2d((2, 4))
+
+        # block3
+        self.conv3 = nn.Conv2d(128, 128, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.activate3 = nn.ELU()
+        self.maxpool3 = nn.MaxPool2d((2, 4))
+
+        # block4
+        self.conv4 = nn.Conv2d(128, 128, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.activate4 = nn.ELU()
+        self.maxpool4 = nn.MaxPool2d((3, 5))
+
+        # block5
+        self.conv5 = nn.Conv2d(128, 64, 3, padding=1)
+        self.bn5 = nn.BatchNorm2d(64)
+        self.activate5 = nn.ELU()
+        self.maxpool5 = nn.MaxPool2d((4, 4))
+        self.hidden = nn.Linear(64, 256)
+
+        self.fc = nn.Linear(64, 10)
+
+    def forward(self, x):
         x = x.permute(0, 2, 1, 3)
         x = self.bn(x)
         x = x.permute(0, 2, 1, 3)
@@ -158,9 +235,6 @@ class CNN_Choi_Slim(Module):
         # x = self.hidden(x)
         x = self.fc(x)
         return x
-
-
-# class RCNN_Choi(Module):
 
 
 if __name__ == '__main__':
