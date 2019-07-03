3e80ed656e5fe79377b44bdb9a8bbb7087f3948a
cheng ying <chengying@tezign.com>
2019-07-03 10:25:17 +0800

    init

---
+++ test.py
@@ -0,0 +1,4 @@
+from util import audio_dataset
+
+audio_datasets = audio_dataset.get_gtzan_datasets()
+print(audio_datasets)
--- util/audio_dataset.py
+++ util/audio_dataset.py
@@ -9,18 +9,24 @@ from PIL import Image
 import torchvision
 import torch
 from util.audio_processor import *
-
+import h5py
+import os
 from glob import glob
 from sklearn.model_selection import train_test_split
+
+audio_mel_h5_path = "audio/audio_mel.hdf5"
 
 
 class GTZANDataSet(Dataset):
     def __init__(self, audio_infos):
         self.audio_infos = audio_infos
+        self.audio_mels = h5py.File(audio_mel_h5_path, 'r')
 
     def __getitem__(self, index):
         audio_info = self.audio_infos[index]
-        mel_spectrum = compute_melgram(audio_info[0])
+        # mel_spectrum = compute_melgram(audio_info[0])
+        mel_spectrum = self.audio_mels[audio_info[0]]
+
         return torch.Tensor(mel_spectrum[0]).float(), torch.LongTensor([audio_info[1]])
 
     def __len__(self):
@@ -29,12 +35,16 @@ class GTZANDataSet(Dataset):
 
 def get_gtzan_datasets():
     genres_path = "../genres/"
-
     audio_paths = glob(genres_path + "*/*.au")
 
     genres_list = list(set([x.split('/')[-2] for x in audio_paths]))
 
     audio_infos = [(x, genres_list.index(x.split('/')[-2])) for x in audio_paths]
+    if not os.path.exists(audio_mel_h5_path):
+        with h5py.File(audio_mel_h5_path, 'w') as f:
+            for audio_path, index in audio_infos:
+                mel_spectrum = compute_melgram(audio_path)
+                f.create_dataset(name=audio_path, data=mel_spectrum)
 
     train_audio_infos, test_audio_infos = train_test_split(audio_infos)
 
