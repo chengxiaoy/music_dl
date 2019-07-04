11ceedaf0af21cbb2573de7c9d33cc0b91d29118
cheng ying <chengying@tezign.com>
2019-07-04 12:51:22 +0800

    add rcnn model

--- train.py
+++ train.py
@@ -59,6 +59,7 @@ def train_model(model, dataloaders, criterion, optimizer, writer, num_epochs=150
                     # backward + optimize only if in training phase
                     if phase == 'train':
                         loss.backward()
+                        nn.utils.clip_grad_norm(model.parameters(), max_norm=10)
                         optimizer.step()
 
                 # statistics
