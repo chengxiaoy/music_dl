0fe58934f07c730902fbd55769e6eb2bc3d82127
e <chengyingjuly@163.com>
2019-11-14 23:18:19 +0800

    cache for mel spectrum

--- train_music163.py
+++ train_music163.py
@@ -67,6 +67,7 @@ def train_model(model, dataloaders, criterion, optimizer, writer, num_epochs=150
                 running_loss += loss.item()
                 sum = torch.sum(preds == labels.byte())
                 running_corrects += sum
+                break
             print("corrects sum {}".format(str(running_corrects)))
             epoch_loss = running_loss / len(dataloaders[phase])
             epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
