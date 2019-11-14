c06d6e69694c1474c5060870e69d90383e4bec6a
cheng ying <chengying@tezign.com>
2019-11-14 17:59:31 +0800

    self-supervised learning

--- util/audio_dataset.py
+++ util/audio_dataset.py
@@ -91,22 +91,17 @@ class SiameseDataSet(Dataset):
 
                     melgram_list = compute_melgram_multi_slice(audio_path)
                     melgram1, melgram2 = sample(melgram_list, 2)
-                    print("index===>" + str(index))
 
-                    print("melgram===>" + audio_path)
+                    print("index {} melgram===>{}".format(index, audio_path))
                     return torch.Tensor(melgram1[0]).float(), torch.Tensor(melgram2[0]).float(), torch.Tensor([1])
                 else:
                     audio_info1, audio_info2 = sample(self.audio_infos, 2)
                     audio_path1, audio_path2 = audio_info1[0], audio_info2[0]
 
-                    print("melgram===>{},{}".format(audio_path1, audio_path2))
-
                     melgram1, melgram2 = choice(compute_melgram_multi_slice(audio_path1)), choice(
                         compute_melgram_multi_slice(audio_path2))
-                    print("index===>" + str(index))
 
-                    print("melgram===>{},{}".format(audio_path1, audio_path2))
-
+                    print("index {} melgram===>{},{}".format(index, audio_path1, audio_path2))
                     return torch.Tensor(melgram1[0]).float(), torch.Tensor(melgram2[0]).float(), torch.Tensor([0])
             except Exception as e:
                 # 读取音频可能报错
