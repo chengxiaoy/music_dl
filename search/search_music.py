1b2b536739661ff2af82e9419a7157595a65324e
cheng ying <chengying@tezign.com>
2019-11-27 18:16:55 +0800

    index

--- search/search_music.py
+++ search/search_music.py
@@ -11,20 +11,21 @@ device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 
 
 
-song_id = 29593213
+song_id = 17616212
 
 path = './audio/data'+str(song_id//20000)+"/"+str(song_id)+'.mp3'
+music_path = '../audio/data'+str(song_id//20000)+"/"+str(song_id)+'.mp3'
 
 # music_path = dowload_song(song_id)
 # music_path1 = '../util/149791.mp3'
 # music_path2 = '../util/392907.mp3'
 
-# model = SiameseModel()
-# model = nn.DataParallel(model)
-# model.load_state_dict(torch.load("music_siamese_50000Nov26_02-52-18.pth", map_location='cpu'))
-# model = model.module
-# model.to(device)
-# model.eval()
+model = SiameseModel()
+model = nn.DataParallel(model)
+model.load_state_dict(torch.load("music_siamese_50000Nov26_02-52-18.pth", map_location='cpu'))
+model = model.module
+model.to(device)
+model.eval()
 
 
 def get_index(feature):
@@ -34,7 +35,7 @@ def get_index(feature):
     return index
 
 
-recall_num = 4
+recall_num = 10
 
 features, paths = joblib.load('vec.pkl')
 features = np.array(features)
@@ -43,8 +44,9 @@ index = get_index(features)
 
 # feature1 = model.forward_once(torch.Tensor(compute_melgram(music_path1)).float()).detach().numpy()
 # feature2 = model.forward_once(torch.Tensor(compute_melgram(music_path2)).float()).detach().numpy()
-# feature = model.forward_once(torch.Tensor(compute_melgram_multi_slice(music_path)[0]).float()).detach().numpy()
-D, I = index.search(np.array([features[paths.index(path)]], dtype=np.float32), recall_num)
+# feature_ = model.forward_once(torch.Tensor(compute_melgram_multi_slice(music_path)[0]).float()).detach().numpy()
+feature = np.array([features[paths.index(path)]])
+D, I = index.search(feature, recall_num)
 # D, I = tree.query(feature, 4)
 # for i, (s_d, s_i) in enumerate(zip(D, I)):
 #     print(s_d[1:])
@@ -58,3 +60,9 @@ print(paths[I[0][0]])
 print(paths[I[0][1]])
 print(paths[I[0][2]])
 print(paths[I[0][3]])
+print(paths[I[0][4]])
+print(paths[I[0][5]])
+print(paths[I[0][6]])
+print(paths[I[0][7]])
+print(paths[I[0][8]])
+print(paths[I[0][9]])
